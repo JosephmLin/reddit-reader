5e85a4b0a9abf8ef65e3d4ff90d491c69ec8bd43
Joseph Lin <josephml@usc.edu>
2015-06-16 17:05:54 -0700

    This currently grabs all the name of the collapse classes. Just needs to alter them when downvoted.

--- src/pruneCommentTree.js
+++ src/pruneCommentTree.js
@@ -15,20 +15,48 @@
 */
 console.log("hidden");
 console.log("tagline should be hidden");
-//$(".expand").hide();
-hideClass = function(className)
+
+collapseGrandParent = function(element)
 {
-	console.log("hideClass called to hide " + className);
-	var elements = document.getElementsByClassName(className);
-	for (var i = 0, length = elements.length; i< length; i++)
+	//console.log("collapseGrandParent called " + element)
+	var name_of_grandparent = element.parent().parent().attr('class');
+	console.log(name_of_grandparent);
+	//Will need to parse through the name of the grandparent to get the unique ID for this class.
+}
+addEventListenersToElement = function(element)
+{
+	//element.click(function()
 	{
-		elements[i].style.display='none';
+		collapseGrandParent($(element));
 	}
 }
+changeDownvoteClass = function(className)
+{
+	for (var iter = 0, elenum = className.length; iter < elenum; iter++)
+	{
+		console.log("change class called to add listener to " + className[iter]);
+		var elements = document.getElementsByClassName(className[iter]);
+		for (var i = 0, length = elements.length; i< length; i++)
+		{
+			if(elements[i] == undefined)
+			{
+				console.log(i)
+				console.log("ELEMENTS BEFORE AND AFTER UNDEFINED.");
+				console.log(elements[i-1]);
+				console.log(elements[i]);
+				console.log(elements[i+10]);
+			}
+			else
+			{
+				addEventListenersToElement(elements[i]);
+			}
+		}
+	}
+	console.log("hideclass complete");
+}
 $(document).ready(function ($) {
-	hideClass('arrow up login-required');
-	hideClass('arrow login-required up');
-	hideClass('arrow down login-required');
-	hideClass('arrow login-required down');
-	hideClass('arrow login-required downmod');
+	var list_of_downvote_names = ['arrow down login-required', 'arrow login-required down', 'arrow login-required downmod'];
+	//hideClass(['arrow up login-required', 'arrow login-required up']);
+	changeDownvoteClass(list_of_downvote_names);
+	//addEventListenersToClass(list_of_downvote_names);
 });
